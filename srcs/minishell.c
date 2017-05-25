d6eb048680edceb24ac394400840ae93a1056b22
Anselme GRUMBACH <agrumbac@e2r12p16.42.fr>
2017-05-25 16:04:28 +0200

    colored makefile : blue octopus yellow shell

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,14 +6,17 @@
 /*   By: agrumbac <agrumbac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/25 14:32:59 by agrumbac          #+#    #+#             */
-/*   Updated: 2017/05/25 14:41:42 by agrumbac         ###   ########.fr       */
+/*   Updated: 2017/05/25 15:41:39 by agrumbac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int		main(void)
+int		main(int ac, char **av, char **envp)
 {
+	(void)ac;
+	(void)av;
+	(void)envp;
 	ft_printf("hi\n");
 	return (0);
 }
